5d0a62935b6e1ffb6d716e8b0376ee5d751f7f67
Matthias Rudnick <mail@matthias-rudnick.de>
2013-05-14 19:06:58 +0200

    added routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
 class PostsController < ApplicationController
   def index
+  	@posts = Post.all
   end
+
+  def create
+  	@post = Post.create(params[:post])
+  	@post.user_id = 1 #To Do - Put in real user
+
+  	if @post.save
+  		flash[:notice] = "Post was successfully created."
+  		redirect_to '/'
+  	else
+
+  	end
+  end
+
+  def show 
+  	@post = Post.find(params[:id])
+  end
+
+
 end

991d2039a1c5fa0a7e0e3717318c85209b280a2d
Matthias Rudnick <mail@matthias-rudnick.de>
2013-05-18 18:25:54 +0200

    add binding pry

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
+
   def index
   	@posts = Post.all
+  end
+
+  def new
+    @post=Post.new
   end
 
   def create
@@ -8,15 +13,31 @@ class PostsController < ApplicationController
   	@post.user_id = 1 #To Do - Put in real user
 
   	if @post.save
+      binding pry
   		flash[:notice] = "Post was successfully created."
   		redirect_to '/'
-  	else
+  	else #validation failure
 
   	end
   end
 
   def show 
   	@post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      flash[:notice] = "You successfully updated your post."
+      redirect_to "/posts/#{@post.id}"
+    else #validation failure
+
+    end
+
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Postit::Application.routes.draw do
   root to: 'posts#index'
 
-  resources :posts, :except => :destroy
+  resources :posts, :except => :destroy do
+  	resources :comments
+  end
+
 end
